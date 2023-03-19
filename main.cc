28ad3c086ce6cef1e338091dec026590395fa866
scottlaboe <scottyl2.718@gmail.com>
2023-03-19 14:36:56 -0400

    add vscode

--- main.cc
+++ main.cc
@@ -15,14 +15,24 @@
 #include <cstdio>
 
 #include "libs/base/led.h"
+#include "libs/base/tasks.h"
 #include "third_party/freertos_kernel/include/FreeRTOS.h"
 #include "third_party/freertos_kernel/include/task.h"
 
-extern "C" [[noreturn]] void app_main(void *param) {
-  (void)param;
-  // Turn on Status LED to show the board is on.
-  LedSet(coralmicro::Led::kStatus, true);
+[[noreturn]] void blink_task(void* param) {
+  auto led_type = static_cast<coralmicro::Led*>(param);
+  bool on = true;
+  while (true) {
+    on = !on;
+    coralmicro::LedSet(*led_type, on);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
 
-  printf("Hello out-of-tree world!\r\n");
+extern "C" void app_main(void* param) {
+  (void)param;
+  auto user_led = coralmicro::Led::kUser;
+  xTaskCreate(&blink_task, "blink_user_led_task", configMINIMAL_STACK_SIZE,
+              &user_led, coralmicro::kAppTaskPriority, nullptr);
   vTaskSuspend(nullptr);
 }
